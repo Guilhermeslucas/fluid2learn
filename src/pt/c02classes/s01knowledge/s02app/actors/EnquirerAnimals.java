0221746719bbd21d902faa9ee9bba0b06b473f0f
Guilherme Lucas da Silva <guilherme.slucas@gmail.com>
2015-03-16 13:51:06 -0300

    acertando EnquirerAnimals

--- src/pt/c02classes/s01knowledge/s02app/actors/EnquirerAnimals.java
+++ src/pt/c02classes/s01knowledge/s02app/actors/EnquirerAnimals.java
@@ -1,5 +1,7 @@
 package pt.c02classes.s01knowledge.s02app.actors;
 
+import java.util.HashMap;
+
 import pt.c02classes.s01knowledge.s01base.impl.BaseConhecimento;
 import pt.c02classes.s01knowledge.s01base.inter.IBaseConhecimento;
 import pt.c02classes.s01knowledge.s01base.inter.IDeclaracao;
@@ -10,40 +12,73 @@ import pt.c02classes.s01knowledge.s01base.inter.IResponder;
 public class EnquirerAnimals implements IEnquirer {
 
 	IResponder responder;
+	IObjetoConhecimento obj;
+    String pergunta;
+    String respostaEsperada;
+    String resposta;
+    int i;
+    String animal;
 	
 	public void connect(IResponder responder) {
 		this.responder = responder;
 	}
 	
 	public boolean discover() {
+		
+		
         IBaseConhecimento bc = new BaseConhecimento();
-        IObjetoConhecimento obj;
-		
-		bc.setScenario("animals");
-        obj = bc.recuperaObjeto("tiranossauro");
+        HashMap<String,String> map=new HashMap<String,String>();
+        
+        /*lista o nome de todos animais do banco de dados*/
+        String animais[] = bc.listaNomes();
+        boolean encontrou = true;
+        
+        for (i = 0; i < animais.length && encontrou; i++) {
+        	animal = animais[i];
+        	obj = bc.recuperaObjeto(animal);
+        	IDeclaracao decl = obj.primeira();
+        	boolean esperado = true;
+        	
+        	while (decl != null && esperado) {
+        		pergunta = decl.getPropriedade();
+        		respostaEsperada = decl.getValor();
+        		
+        		if(!map.containsKey(pergunta)){
+					resposta = responder.ask(pergunta);
+					map.put(pergunta, resposta);
 
-		IDeclaracao decl = obj.primeira();
-		
-        boolean animalEsperado = true;
-		while (decl != null && animalEsperado) {
-			String pergunta = decl.getPropriedade();
-			String respostaEsperada = decl.getValor();
-			
-			String resposta = responder.ask(pergunta);
-			if (resposta.equalsIgnoreCase(respostaEsperada))
-				decl = obj.proxima();
-			else
-				animalEsperado = false;
-		}
-		
-		boolean acertei = responder.finalAnswer("tiranossauro");
-		
-		if (acertei)
-			System.out.println("Oba! Acertei!");
-		else
-			System.out.println("fuem! fuem! fuem!");
-		
-		return acertei;
+				}
+				else
+					//ja fez a pergunta
+					resposta = map.get(pergunta);
+					
+        		if (resposta.equalsIgnoreCase(respostaEsperada)) {
+        			decl = obj.proxima();
+        		}
+        		
+        		else {
+        			esperado = false;
+        		}
+        	}
+        		
+        	
+        	if (esperado) {
+        		encontrou = false;
+        		
+        	}
+        }
+ 
+        boolean acertei = responder.finalAnswer(animal);
+        
+        if (acertei) {
+        	System.out.println("Oba! Acertei!");
+        	return true;
+        }
+        
+        else {
+        	System.out.println("fuem! fuem! fuem");
+        	return false;
+        }
+        
 	}
-
 }
